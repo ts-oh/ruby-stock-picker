636b77f9d891243153d698b7285bb3d006511637
ts-oh <tsohlove@gmail>
2021-09-18 16:39:54 +0900

    initial commit

---
+++ sp.rb
@@ -0,0 +1,9 @@
+#require 'pry-byebug'
+
+def stock_picker(stocks); end
+
+#pick low num and highest num going linear step through day
+#find the the two days (indexes) with biggest difference
+
+# expected => [1,4] for a profit of $15 - $3 == $12
+stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
