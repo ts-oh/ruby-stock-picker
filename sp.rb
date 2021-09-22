374aa43ad6f4c42cd0ebab623be975cbc7aae67f
ts-oh <tsohlove@gmail.com>
2021-09-22 11:04:20 +0900

    new approach to the solution

--- sp.rb
+++ sp.rb
@@ -1,9 +1,21 @@
-#require 'pry-byebug'
+# require 'pry-byebug'
 
-def stock_picker(stocks); end
+stocks = [17, 3, 6, 9, 15, 8, 6, 1, 10]
 
-#pick low num and highest num going linear step through day
-#find the the two days (indexes) with biggest difference
+# stock picker method takes array of stock prices as parameter
+def stock_picker(stocks)
+  stocks.each_with_index do |_, index|
+    new_array = []
+    stocks[index..-1].each do |value|
+      new_array.push(value)
+    end
+    p new_array
+  end
+
+    # once first element is finished do same for second element for the whole array
+    # find the largest profit difference
+    # return the index number that gave largest profit difference
+end
 
 # expected => [1,4] for a profit of $15 - $3 == $12
 stock_picker([17, 3, 6, 9, 15, 8, 6, 1, 10])
